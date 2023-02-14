1ffb5d46cad59aba82211c1969800cf44177e433
bodhichristian <110639779+bodhichristian@users.noreply.github.com>
2023-02-14 18:57:14 -0500

    AskAI Update
    Refactoring

--- AskAI/Views/HomeView/HomeView.swift
+++ AskAI/Views/HomeView/HomeView.swift
@@ -41,185 +41,19 @@ struct HomeView: View {
         }
         return dallEFilter.count
     }
-
     
     var body: some View {
         NavigationView {
             VStack {
-                CircleImage(imageName: "askAI-logo", width: 200, height: 200)
-                    .shadow(color: .secondary, radius: diminishingShadowRadius)
-                    .shadow(color: profile.theme.mainColor, radius: expandingShadowRadius)
-                    .padding(15)
+                // Welcome View: Circle Image and Greeting
+                welcomeView
                 
-                HStack(spacing: 0) {
-                    Text("Hello, ")
-                    Text("\(profile.username)")
-                        .foregroundColor(profile.theme.mainColor)
-                    Text(".")
-                }
-                .bold()
-                .font(.title)
+                // Dashboard View: User stat block
+                dashboardView
                 
-                Text("What will we create today?")
-                    .opacity(showingWelcomeMessage ? 1 : 0)
-                    .offset(y: fallingOffset)
-                    .padding(.bottom)
-                
-                
-                // Dashboard section
-                VStack {
-                    HStack {
-                        Text("Dashboard")
-                            .font(.headline)
-                            .offset(y: fallingOffset)
-                            .padding([.leading, .top])
-                        Spacer()
-                    }
-                    
-                    
-                    HStack {
-                        VStack {
-                            HStack(alignment: .center) {
-                                Text("\(totalRequests.count)")
-                                    .font(.largeTitle)
-                                    .fontWeight(.semibold)
-                                    .offset(y: risingOffset)
-                                
-                                Image(systemName: "questionmark.bubble.fill")
-                                    .resizable()
-                                    .frame(width: 40, height: 40)
-                                    .foregroundColor(.green)
-                                    .offset(y: fallingOffset)
-                                
-                            }
-                            Text("AskAI Requests")
-                                .font(.footnote)
-
-                        }
-                        
-                        Spacer()
-                        
-                        VStack {
-                            HStack(alignment: .center) {
-                                Text("\(savedChatCount)")
-                                    .font(.largeTitle)
-                                    .fontWeight(.semibold)
-                                    .offset(y: risingOffset)
-                                
-                                
-                                Image(systemName: "quote.bubble.fill")
-                                    .resizable()
-                                    .frame(width: 40, height: 40)
-                                    .foregroundColor(.purple)
-                                    .offset(y: fallingOffset)
-                                
-                            }
-                            Text("Saved chats")
-                                .font(.footnote)
-                        }
-
-                        Spacer()
-                        
-                        VStack {
-                            HStack(alignment: .center) {
-                                Text("\(savedImageCount)")
-                                    .font(.largeTitle)
-                                    .fontWeight(.semibold)
-                                    .offset(y: risingOffset)
-                                
-                                Image(systemName: "rectangle.3.group.bubble.left.fill")
-                                    .resizable()
-                                    .frame(width: 40, height: 40)
-                                    .foregroundColor(.blue)
-                                    .offset(y: fallingOffset)
-                                
-                            }
-                            Text("Saved images")
-                                .font(.footnote)
-
-                        }
-                    }
-                    .padding(.horizontal)
-                    .padding(.bottom)
-                }
-                .background(
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 10)
-                            .foregroundColor(profile.theme.mainColor.opacity(0.2))
-//                        RoundedRectangle(cornerRadius: 10)
-//                            .foregroundStyle(.ultraThinMaterial)
-                    }
-
-                )
-                .padding(.horizontal)
-                
-                // Join the conversation section
-                VStack {
-                    HStack {
-                        Text("Join the Conversation")
-                            .font(.headline)
-                            .offset(y: fallingOffset)
-     
-                            .padding([.leading, .top])
-                        Spacer()
-                    }
-                    
-                    HStack {
-                        // Reddit Link Stack
-                        HStack {
-                            Image("reddit")
-                                .resizable().scaledToFit()
-                                .frame(width: 40)
-                                .offset(y: fallingOffset)
-                            VStack(alignment: .leading, spacing: 6) {
-                                Link(destination: redditChatGPT) {
-                                    Text("r/ChatGPT")
-                                        .offset(y: fallingOffset)
-                                }
-                                
-                                    Link(destination: redditChatGPT) {
-                                        Text("r/dalle2")
-                                            .offset(y: risingOffset)
-                                }
-                            }
-                        }
-                        
-                        Spacer()
-                        
-                        // Twitter Link Stack
-                        HStack {
-                            Image("twitter")
-                                .resizable().scaledToFit()
-                                .frame(width: 40)
-                                .offset(y: fallingOffset)
-                            VStack(alignment: .leading, spacing: 6) {
-                                Link(destination: twitterChatGPT) {
-                                    Text("#chatGPT")
-                                        .offset(y: fallingOffset)
-                                }
-                                    Link(destination: twitterDALLE) {
-                                        Text("#dalle2")
-                                            .offset(y: risingOffset)
-                                }
-                            }
-                        }
-
-                    }
-                    .padding(.horizontal)
-                    .padding(.bottom)
-                }
-                .background(
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 10)
-                            .foregroundColor(profile.theme.mainColor.opacity(0.2))
-//                        RoundedRectangle(cornerRadius: 10)
-//                            .foregroundStyle(.ultraThinMaterial)
-                    }
-
-                )
-                .padding()
+                //Conversation View: Reddit & Twitter links
+                conversationView
             }
-            
             .navigationTitle(Text("AskAI"))
             .onAppear {
                 withAnimation(.easeInOut(duration: 0.8)) {
@@ -237,13 +71,188 @@ struct HomeView: View {
                     Image(systemName: "gear")
                 }
             }
-
         }
         .sheet(isPresented: $showingSettingsView) {
             SettingsView(userTheme: $profile.theme)
                 .environmentObject(profile)
         }
+    }
+}
 
+extension HomeView {
+    private var welcomeView: some View {
+        VStack {
+            CircleImage(imageName: "askAI-logo", width: 200, height: 200)
+                .shadow(color: .secondary, radius: diminishingShadowRadius)
+                .shadow(color: profile.theme.mainColor, radius: expandingShadowRadius)
+                .padding(15)
+            
+            // Greeting:
+            // Hello, <user>.
+            // What will we create today?
+            HStack(spacing: 0) {
+                Text("Hello, ")
+                Text("\(profile.username)")
+                    .foregroundColor(profile.theme.mainColor)
+                Text(".")
+            }
+            .bold()
+            .font(.title)
+            
+            Text("What will we create today?")
+                .opacity(showingWelcomeMessage ? 1 : 0)
+                .offset(y: fallingOffset)
+                .padding(.bottom)
+        }
+    }
+    
+    private var dashboardView: some View {
+        VStack {
+            HStack {
+                Text("Dashboard")
+                    .font(.headline)
+                    .offset(y: fallingOffset)
+                    .padding([.leading, .top])
+                
+                Spacer()
+            }
+            
+            HStack {
+                VStack {
+                    HStack(alignment: .center) {
+                        Text("\(totalRequests.count)")
+                            .font(.largeTitle)
+                            .fontWeight(.semibold)
+                            .offset(y: risingOffset)
+                        
+                        Image(systemName: "questionmark.bubble.fill")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .foregroundColor(.green)
+                            .offset(y: fallingOffset)
+                    }
+                    
+                    Text("AskAI Requests")
+                        .font(.footnote)
+                }
+                
+                Spacer()
+                
+                VStack {
+                    HStack(alignment: .center) {
+                        Text("\(savedChatCount)")
+                            .font(.largeTitle)
+                            .fontWeight(.semibold)
+                            .offset(y: risingOffset)
+                        
+                        Image(systemName: "quote.bubble.fill")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .foregroundColor(.purple)
+                            .offset(y: fallingOffset)
+                    }
+                    
+                    Text("Saved chats")
+                        .font(.footnote)
+                }
+
+                Spacer()
+                
+                VStack {
+                    HStack(alignment: .center) {
+                        Text("\(savedImageCount)")
+                            .font(.largeTitle)
+                            .fontWeight(.semibold)
+                            .offset(y: risingOffset)
+                        
+                        Image(systemName: "rectangle.3.group.bubble.left.fill")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .foregroundColor(.blue)
+                            .offset(y: fallingOffset)
+                        
+                    }
+                    
+                    Text("Saved images")
+                        .font(.footnote)
+                }
+            }
+            .padding(.horizontal)
+            .padding(.bottom)
+        }
+        .background(
+            ZStack{
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(profile.theme.mainColor.opacity(0.2))
+            }
+        )
+        .padding(.horizontal)
+    }
+    
+    private var conversationView: some View {
+        VStack {
+            HStack {
+                Text("Join the Conversation")
+                    .font(.headline)
+                    .offset(y: fallingOffset)
+
+                    .padding([.leading, .top])
+                Spacer()
+            }
+            
+            HStack {
+                // Reddit Link Stack
+                HStack {
+                    Image("reddit")
+                        .resizable().scaledToFit()
+                        .frame(width: 40)
+                        .offset(y: fallingOffset)
+                    VStack(alignment: .leading, spacing: 6) {
+                        Link(destination: redditChatGPT) {
+                            Text("r/ChatGPT")
+                                .offset(y: fallingOffset)
+                        }
+                        
+                            Link(destination: redditChatGPT) {
+                                Text("r/dalle2")
+                                    .offset(y: risingOffset)
+                        }
+                    }
+                }
+                
+                Spacer()
+                
+                // Twitter Link Stack
+                HStack {
+                    Image("twitter")
+                        .resizable().scaledToFit()
+                        .frame(width: 40)
+                        .offset(y: fallingOffset)
+                    VStack(alignment: .leading, spacing: 6) {
+                        Link(destination: twitterChatGPT) {
+                            Text("#chatGPT")
+                                .offset(y: fallingOffset)
+                        }
+                            Link(destination: twitterDALLE) {
+                                Text("#dalle2")
+                                    .offset(y: risingOffset)
+                        }
+                    }
+                }
+
+            }
+            .padding(.horizontal)
+            .padding(.bottom)
+        }
+        .background(
+            ZStack{
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(profile.theme.mainColor.opacity(0.2))
+
+            }
+
+        )
+        .padding()
     }
 }
 
--- AskAI/Views/MainView/MainView.swift
+++ AskAI/Views/MainView/MainView.swift
@@ -11,22 +11,29 @@ struct MainView: View {
     @EnvironmentObject var savedChats: SavedChats
     @ObservedObject var profile: Profile
     @ObservedObject var totalRequests: TotalRequests
+    
+    // App will launch to selectedTab
     @State private var selectedTab = 1
 
     var body: some View {
         TabView(selection: $selectedTab) {
+            
+            // ChatGPT Tab
             ChatGPTMainView(totalRequests: totalRequests, savedChats: savedChats)
                 .tabItem {
                     Label("ChatGPT", systemImage: "character.cursor.ibeam")
                 }
                 .tag(0)
             
+            // Home Tab
             HomeView(totalRequests: totalRequests, savedChats: savedChats, profile: profile)
                 .tabItem {
                     Label("Home", systemImage: "rotate.3d")
                 }
                 .tag(1)
             
+            
+            // DALL·E Tab
             DALLEMainView(savedChats: savedChats, totalRequests: totalRequests)
                 .tabItem {
                     Label("DALL-E", systemImage: "bubbles.and.sparkles.fill")
